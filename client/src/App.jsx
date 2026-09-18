4fa93de01d86aad558f84eb235dcd0b3d0560e1f
serverless <vasusahu4008@gmail.com>
2026-09-18 21:21:49 +0530

    working on frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,28 @@
-import AddProblemForm from "./components/progressData/addProblemForm"
-import CFDashboard from "./components/Dashboards/CFDashboard"
-import GoalProgress from "./components/progressData/GoalProgress"
-import LeetcodeDashboard from "./components/Dashboards/LeetcodeDashboard"
-import RatingCharts from "./components/RatingChart/CodeforcesRating"
-import CodechefDashboard from "./components/Dashboards/CodechefDashboard"
+// import AddProblemForm from "./components/progressData/addProblemForm"
+// import CFDashboard from "./components/Dashboards/CFDashboard"
+// import GoalProgress from "./components/progressData/GoalProgress"
+// import LeetcodeDashboard from "./components/Dashboards/LeetcodeDashboard"
+// import RatingCharts from "./components/RatingChart/CodeforcesRating"
+// import CodechefDashboard from "./components/Dashboards/CodechefDashboard"
 
 
 function App() {
 
   return (
     <div>
-      <CFDashboard handle="Vasu.609"/>
+      hey there
+
+      <div className="justify-center b-2" >
+
+        <input type="text"  placeholder="write your username" className="b-2  text-center"/>
+        <button>find</button>
+      </div>
+      {/* <CFDashboard handle="Vasu.609"/>
       <AddProblemForm/>
       <GoalProgress/>
       <RatingCharts handle="Vasu.609"/>
       <LeetcodeDashboard handle="VasuS609"/>
-      <CodechefDashboard handle="vash609"/>
+      <CodechefDashboard handle="vash609"/> */}
     </div>
   )
   
